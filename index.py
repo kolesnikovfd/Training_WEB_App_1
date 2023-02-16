81b46e0d0aeeb6e329029f58b4c944ab71e41c97
koleso_O <kolesnikovfd@gmail.com>
2023-02-16 19:57:18 +0400

    2. Тренировки в полёте

--- index.py
+++ index.py
@@ -5,39 +5,16 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-@app.route('/')
-@app.route('/index')
-def index():
-    param = {}
-    param['title'] = 'Заготовка'
-    return render_template('index.html', **param)
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('index.html', title=title)
 
 
-@app.route('/odd_even')
-def odd_even():
-    return render_template('odd_even.html', number=2)
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
 
-
-@app.route('/news')
-def news():
-    with open("news.json", "rt", encoding="utf8") as f:
-        news_list = json.loads(f.read())
-    print(news_list)
-    return render_template('news.html', news=news_list)
-
-
-@app.route('/queue')
-def queue():
-    return render_template('queue.html')
-
-'''
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Авторизация', form=form)
-'''
 
 if __name__ == '__main__':
     app.run(port='8080', host='127.0.0.1')
