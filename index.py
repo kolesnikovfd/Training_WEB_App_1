3d18ba9cc05bacd155d7260b205d35b59813b283
koleso_O <kolesnikovfd@gmail.com>
2023-02-16 19:30:56 +0400

    1. Готовимся к миссии

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import json
-
-from flask import Flask, render_template
+#from loginform import LoginForm
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -9,8 +9,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/index')
 def index():
     param = {}
-    param['username'] = "Ученик Яндекс.Лицея"
-    param['title'] = 'Домашняя страница'
+    param['title'] = 'Заготовка'
     return render_template('index.html', **param)
 
 
@@ -31,14 +30,14 @@ def news():
 def queue():
     return render_template('queue.html')
 
-
+'''
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         return redirect('/success')
     return render_template('login.html', title='Авторизация', form=form)
-
+'''
 
 if __name__ == '__main__':
     app.run(port='8080', host='127.0.0.1')
