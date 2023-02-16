b221a42ca39b083843b9f169c4f4950bf5cad741
koleso_O <kolesnikovfd@gmail.com>
2023-02-16 19:12:16 +0400

    Initial commit

---
+++ index.py
@@ -0,0 +1,44 @@
+import json
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+@app.route('/')
+@app.route('/index')
+def index():
+    param = {}
+    param['username'] = "Ученик Яндекс.Лицея"
+    param['title'] = 'Домашняя страница'
+    return render_template('index.html', **param)
+
+
+@app.route('/odd_even')
+def odd_even():
+    return render_template('odd_even.html', number=2)
+
+
+@app.route('/news')
+def news():
+    with open("news.json", "rt", encoding="utf8") as f:
+        news_list = json.loads(f.read())
+    print(news_list)
+    return render_template('news.html', news=news_list)
+
+
+@app.route('/queue')
+def queue():
+    return render_template('queue.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port='8080', host='127.0.0.1')
