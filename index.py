d0bb0b7863cd38103470a05f4dc8a09edab07917
koleso_O <kolesnikovfd@gmil.com>
2023-03-22 01:27:35 +0400

    7. Журнал работ (Доработанная)

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import json
 import datetime
+
+from forms.user import RegisterForm
 from loginform import LoginForm
 from flask import Flask, render_template, redirect
 from data import db_session, __all_models
@@ -67,29 +69,40 @@ def table(sex, age):
 def works_log():
     db_session.global_init("db/mars_explorer.db")
     db_sess = db_session.create_session()
-    print(1)
     jobs = db_sess.query(Jobs).all()
-    print(*jobs)
     for job in jobs:
-        print(job.team_leader)
         tl = db_sess.query(User).filter(User.id == job.team_leader).first()
         job.team_leader = f'{tl.surname} {tl.name}'
-        print(4)
-    """
-    params = {'title': 'Список работ',
-              'jobs': []
-              }
-        team_leader = 
-        params['jobs'].append({
-            'id': job.id,
-            'job': job.job,
-            'team_leader': f'{team_leader.surname} {team_leader.name}',
-            'duration': {job.end_date - job.start_date} if job.is_finished else {datetime.datetime.now() - job.start_date},
-            'collaborators': job.collaborators,
-            'is_finised': job.is_finished
-        })
-    """
     return render_template('works_log.html', jobs=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            email=form.email.data,
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 def search():
